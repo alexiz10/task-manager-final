744b6aeac15884f8e517b6ffac3929bfcb51b017
Alexiz Hernandez <alexiz_hernandez@yahoo.com>
2018-03-02 08:05:07 -0700

    Added class variables to TaskManager

--- src/TaskManager.java
+++ src/TaskManager.java
@@ -1,7 +1,14 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class TaskManager {
 
+    private Scanner scanner = new Scanner(System.in);
+    private ArrayList<Task> tasks = new ArrayList<>();
+    private ArrayList<Task> incompleteTasks = new ArrayList<>();
+
     public void start() {
-        
+
     }
 
 }
