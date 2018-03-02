263fbb50be44097029afe58ae0ef04eddd2b6d0b
Alexiz Hernandez <alexiz_hernandez@yahoo.com>
2018-03-02 08:12:28 -0700

    Added helper methods for user input

--- src/TaskManager.java
+++ src/TaskManager.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class TaskManager {
@@ -8,6 +9,8 @@ public class TaskManager {
     private ArrayList<Task> incompleteTasks = new ArrayList<>();
 
     private final String Q_WHAT_TO_DO = "What would you like to do?";
+
+    private final String ERR_MUST_BE_A_NUMBER = "Error: Entry must be a number.";
 
     private final String NEW_LINE = "\n";
 
@@ -23,11 +26,32 @@ public class TaskManager {
             NEW_LINE + Q_WHAT_TO_DO;
 
     public void start() {
-        
+
     }
 
     private void printMenu() {
 
+    }
+
+    private int inputInt(String prompt, Scanner scanner) {
+        System.out.println(prompt);
+        try {
+            return scanner.nextInt();
+        } catch (InputMismatchException e) {
+            System.out.println(ERR_MUST_BE_A_NUMBER);
+            scanner.next();
+            return inputInt(prompt, scanner);
+        }
+    }
+
+    private String inputString(String prompt, Scanner scanner) {
+        System.out.println(prompt);
+        String input = "";
+        scanner.nextLine();
+        if (scanner.hasNext()) {
+            input = scanner.nextLine();
+        }
+        return input;
     }
 
 }
